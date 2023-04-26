a809319f58267fc9856f83b766318e6f1768fc45
Mahadi90 <mahadimhs787@gmail.com>
2023-04-26 23:03:48 +0600

    news data load added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json')
 
+const news = require('./data/news.json')
+
 app.get('/', (req, res) => {
     res.send('Mahadi news is running')
 })
@@ -13,6 +15,28 @@ app.use(cors())
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id)
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if(id === 0 ){
+        res.send(news)
+    }else{
+        const newsCategory = news.filter(n => parseInt(n.category_id) === id)
+        res.send(newsCategory)
+    }
+    
 })
 
 app.listen(port , () => {
